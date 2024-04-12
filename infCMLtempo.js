48c8eb062e8c3c80ad1f7bc88c87fca3511e2c22
Cynder <114823140+TheAceburn@users.noreply.github.com>
2024-04-12 19:20:09 -0300

    Create infCMLtempo.js

---
+++ infCMLtempo.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Infinite Chrome Music Lab Tempo
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  removes the tempo limits on the chrome music lab song maker although it breaks the slider
+// @author       cynder using CHATGPT????!!!?!!?! :scream::scream:
+// @match        https://musiclab.chromeexperiments.com/Song-Maker/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Get all input elements with class "input-number" and name "tempo"
+    var inputs = document.querySelectorAll('input.input-number[name="tempo"]');
+
+    // Loop through each input element
+    inputs.forEach(function(input) {
+        // Update min attribute to -Infinity
+        input.setAttribute('min', '-Infinity');
+        // Update max attribute to Infinity
+        input.setAttribute('max', 'Infinity');
+    });
+})();
